f051500154d8cd83b2dbb13103898a2d003ba9c6
Muaaz ul Hassan <74170095+muaaz-0@users.noreply.github.com>
2024-05-23 08:09:36 -0700

    Update control_monitor_gpios_with_webserver.ino

--- control_monitor_gpios_with_webserver.ino
+++ control_monitor_gpios_with_webserver.ino
@@ -5,8 +5,6 @@
 #include "Arduino.h"
 #include "Audio.h"
 
-//----
-
 
 #if CONFIG_FREERTOS_UNICORE
 static const BaseType_t app_cpu=0;
@@ -18,14 +16,11 @@ static TaskHandle_t task_Speaker_Handle= NULL ;
 static TaskHandle_t task_Web_Handle= NULL ;
 
 
-//------
 #define I2S_DOUT  27
 #define I2S_BCLK  26
 #define I2S_LRC   25
 
 Audio audio;
-
-
 
 #define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 64 
@@ -104,8 +99,8 @@ void task_Speaker(void *parameter){
 void task_Web(void *parameter){
   
  while(1){
-
-    //chk in/out
+vTaskDelay( 1000/ portTICK_PERIOD_MS);
+  
   input36state=digitalRead(input36) ? "HIGH" : "LOW";
   input39state=digitalRead(input39) ? "HIGH" : "LOW";
   input34state=digitalRead(input34) ? "HIGH" : "LOW";
@@ -142,41 +137,41 @@ void task_Web(void *parameter){
           
 
             if (header.indexOf("GET /23/on") >= 0) {
-            //  Serial.println(F("GPIO 23 on"));
+           
             
               output23State = "on";
               digitalWrite(output23, HIGH);
             }
             
              else if (header.indexOf("GET /23/off") >= 0) {
-           //   Serial.println(F("GPIO 23 off"));
+         
             
               output23State = "off";
               digitalWrite(output23, LOW); 
              }
               else if (header.indexOf("GET /2/on") >= 0) {
-          //    Serial.println(F("GPIO 2 on"));
+         
           display.clearDisplay();
           display.setCursor(0, 10);
-           display.println(F("GPIO 2 on"));
-    display.display();
+          display.println(F("GPIO 2 on"));
+          display.display();
     
               output2State = "on";
               digitalWrite(output2, HIGH);
             }
                 else if (header.indexOf("GET /2/off") >= 0) {
-          //    Serial.println(F("GPIO 2 off"));
+         
           display.clearDisplay();
           display.setCursor(0, 10);
           display.println(F("GPIO 2 off"));
-    display.display();
-              output2State = "off";
-              digitalWrite(output2, LOW);
+          display.display();
+          output2State = "off";
+          digitalWrite(output2, LOW);
                
           
             }
             else if (header.indexOf("GET /1/on") >= 0) {
-          //    Serial.println(F("GPIO 1 on"));
+        
               output1State = "on";
               digitalWrite(output3, HIGH);
             } 
@@ -186,89 +181,89 @@ void task_Web(void *parameter){
               digitalWrite(output1, LOW);
               }
              
-            else if (header.indexOf("GET /3/on") >= 0) {
-         //     Serial.println(F("GPIO 3 on"));
+              else if (header.indexOf("GET /3/on") >= 0) {
+        
               output3State = "on";
               digitalWrite(output3, HIGH);
             } 
               else if (header.indexOf("GET /3/off") >= 0) {
-         //     Serial.println(F("GPIO  off"));
+      
               output3State = "off";
               digitalWrite(output3, LOW);
               }
               else if (header.indexOf("GET /19/on") >= 0) {
-          //    Serial.println(F("GPIO 19 on"));
+        
               output19State = "on";
               digitalWrite(output19, HIGH);
             }
               
-            else if (header.indexOf("GET /19/off") >= 0) {
-        //      Serial.println(F("GPIO 19 off"));
+              else if (header.indexOf("GET /19/off") >= 0) {
+       
               output19State = "off";
               digitalWrite(output19, LOW);
-            }//for led2
-            else if (header.indexOf("GET /18/on") >= 0) {
-         //     Serial.println(F("GPIO 18 on"));
+            }
+              else if (header.indexOf("GET /18/on") >= 0) {
+         
               output18State = "on";
               digitalWrite(output18, HIGH);
             }
-            else if (header.indexOf("GET /18/off") >= 0) {
-        //      Serial.println(F("GPIO 18 off"));
+              else if (header.indexOf("GET /18/off") >= 0) {
+      
               output18State = "off";
               digitalWrite(output18, LOW);
             }
-            else if (header.indexOf("GET /5/on") >= 0) {
-        //      Serial.println(F("GPIO 5 on"));
+              else if (header.indexOf("GET /5/on") >= 0) {
+    
               output5State = "on";
               digitalWrite(output5, HIGH);
             }
              
-            else if (header.indexOf("GET /5/off") >= 0) {
-      //        Serial.println(F("GPIO 5 off"));
+             else if (header.indexOf("GET /5/off") >= 0) {
+  
               output5State = "off";
               digitalWrite(output5, LOW);
             }
              else if (header.indexOf("GET /17/on") >= 0) {
-      //        Serial.println(F("GPIO 17 on"));
+     
               output17State = "on";
               digitalWrite(output17, HIGH);
              
             } else if (header.indexOf("GET /17/off") >= 0) {
-      //        Serial.println(F("GPIO 17 off"));
+   
               output17State = "off";
               digitalWrite(output17, LOW);
             }
-               else if (header.indexOf("GET /16/on") >= 0) {
-      //        Serial.println(F("GPIO 16 on"));
+              else if (header.indexOf("GET /16/on") >= 0) {
+  
               output16State = "on";
               digitalWrite(output16, HIGH);
              
             } else if (header.indexOf("GET /16/off") >= 0) {
-     //         Serial.println(F("GPIO 16 off"));
+
               output16State = "off";
               digitalWrite(output16, LOW);
             }
-                  else if (header.indexOf("GET /4/on") >= 0) {
-     //         Serial.println(F("GPIO 4 on"));
+              else if (header.indexOf("GET /4/on") >= 0) {
+   
               output4State = "on";
               digitalWrite(output4, HIGH);
              
             } else if (header.indexOf("GET /4/off") >= 0) {
-    //          Serial.println(F("GPIO 4 off"));
+    
               output4State = "off";
               digitalWrite(output4, LOW);
             }
-                  else if (header.indexOf("GET /15/on") >= 0) {
-    //          Serial.println(F("GPIO 15 on"));
+              else if (header.indexOf("GET /15/on") >= 0) {
+   
               output15State = "on";
               digitalWrite(output15, HIGH);
              
-            }  else if (header.indexOf("GET /15/off") >= 0) {
-   //           Serial.println(F("GPIO 15 off"));
+            } else if (header.indexOf("GET /15/off") >= 0) {
+
               output15State = "off";
               digitalWrite(output15, LOW);
             }
-            // Display the HTML web page
+            
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<meta http-equiv=\"refresh\" content=\"7\">"); 
@@ -279,66 +274,65 @@ void task_Web(void *parameter){
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #555555;}</style></head>");
             
-            // Web Page Heading
+            
             client.println("<body><h1>ESP32 Web Server</h1>");
                  //2 displaying state of led2
             client.println("<p>GPIO 2 - State " + output2State + "</p>");
             
             if (output2State=="off") {
-              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+            client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
-         //23
+        
             client.println("<p>GPIO 23 - State " + output23State + "</p>");
             
             if (output23State=="off") {
-              client.println("<p><a href=\"/23/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("<p><a href=\"/23/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/23/off\"><button class=\"button button2\">OFF</button></a></p>");
+            client.println("<p><a href=\"/23/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
-         //1
+        
             client.println("<p>GPIO 1 - State " + output1State + "</p>");
             
             if (output1State=="off") {
-              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+            client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
-          //3
-             client.println("<p>GPIO 3 - State " + output3State + "</p>");
+        
+            client.println("<p>GPIO 3 - State " + output3State + "</p>");
             
             if (output3State=="off") {
-              client.println("<p><a href=\"/3/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("<p><a href=\"/3/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/3/off\"><button class=\"button button2\">OFF</button></a></p>");
+            client.println("<p><a href=\"/3/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
-           //19
-              client.println("<p>GPIO 19 - State " + output19State + "</p>");
+            client.println("<p>GPIO 19 - State " + output19State + "</p>");
             
             if (output19State=="off") {
-              client.println("<p><a href=\"/19/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("<p><a href=\"/19/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/19/off\"><button class=\"button button2\">OFF</button></a></p>");
+            client.println("<p><a href=\"/19/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
-            //18
-               client.println("<p>GPIO 18 - State " + output18State + "</p>");
+          
+            client.println("<p>GPIO 18 - State " + output18State + "</p>");
             
             if (output18State=="off") {
-              client.println("<p><a href=\"/18/on\"><button class=\"button\">ON</button></a></p>");
+            client.println("<p><a href=\"/18/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/18/off\"><button class=\"button button2\">OFF</button></a></p>");
+            client.println("<p><a href=\"/18/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
-            //5
-               client.println("<p>GPIO 5 - State " + output5State + "</p>");
+            
+            client.println("<p>GPIO 5 - State " + output5State + "</p>");
             
             if (output5State=="off") {
               client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
-            //17
+         
 
                client.println("<p>GPIO 17 - State " + output17State + "</p>");
             
@@ -347,7 +341,7 @@ void task_Web(void *parameter){
             } else {
               client.println("<p><a href=\"/17/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
-             //16
+           
 
                client.println("<p>GPIO 16 - State " + output16State + "</p>");
             
@@ -357,7 +351,7 @@ void task_Web(void *parameter){
               client.println("<p><a href=\"/16/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
-           //4------
+        
 
             client.println("<p>GPIO 4 - State " + output4State + "</p>");
      
@@ -369,7 +363,7 @@ void task_Web(void *parameter){
 
        
 
-            //15------
+           
 
             client.println("<p>GPIO 15 - State " + output15State + "</p>");
      
@@ -380,7 +374,7 @@ void task_Web(void *parameter){
             }
 
             
-            //
+            
             client.println("<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"icon\" href=\"data:,\"></head><body>");
             client.print("<p>Input Pin 36 - State: ");
             client.print(input36state);
@@ -464,12 +458,12 @@ void task_Web(void *parameter){
 
 void setup() {
   
-  Serial.begin(1000000);
-  Serial.print(F("Connecting to "));
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    vTaskDelay( 5000/ portTICK_PERIOD_MS);
+    Serial.begin(1000000);
+    Serial.print(F("Connecting to "));
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+    vTaskDelay( 1000/ portTICK_PERIOD_MS);
     Serial.print(".");
   }
   //pin config i2s
@@ -483,16 +477,16 @@ void setup() {
   //audio.connecttohost("stream.1a-webradio.de/deutsch/mp3-128/vtuner-1a");
   //audio.connecttohost("www.antenne.de/webradio/antenne.m3u");
   audio.connecttohost("0n-80s.radionetz.de:8000/0n-70s.mp3");
-//    audio.connecttohost("https://www.youtube.com/watch?v=95yUbClyf3E&list=PLXyB2ILBXW5FLc7j2hLcX6sAGbmH0JxX8&index=3");
+  //audio.connecttohost("https://www.youtube.com/watch?v=95yUbClyf3E&list=PLXyB2ILBXW5FLc7j2hLcX6sAGbmH0JxX8&index=3");
 
    
  
-//for OLED init
+
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
- vTaskDelay( 2000/ portTICK_PERIOD_MS);
+  TaskDelay( 2000/ portTICK_PERIOD_MS);
   
   display.clearDisplay();
 
@@ -503,7 +497,7 @@ void setup() {
    
 
      
-     //wifi conn
+   //wifi conn
   Serial.println("");
   Serial.println(F("WiFi connected."));
   Serial.println(F("IP address: "));
@@ -511,30 +505,29 @@ void setup() {
   server.begin();
 
   
-  
-  pinMode(output23, OUTPUT);
-  pinMode(output1, OUTPUT);
-  pinMode(output3, OUTPUT);
-  pinMode(output19, OUTPUT);
-  pinMode(output18, OUTPUT);
-  pinMode(output5, OUTPUT);
-  pinMode(output17, OUTPUT);
-  pinMode(output16, OUTPUT);
-  pinMode(output4, OUTPUT);
-  pinMode(output2, OUTPUT);
-  pinMode(output15, OUTPUT);
-  //-----------------------
+    
+    pinMode(output23, OUTPUT);
+    pinMode(output1, OUTPUT);
+    pinMode(output3, OUTPUT);
+    pinMode(output19, OUTPUT);
+    pinMode(output18, OUTPUT);
+    pinMode(output5, OUTPUT);
+    pinMode(output17, OUTPUT);
+    pinMode(output16, OUTPUT);
+    pinMode(output4, OUTPUT);
+    pinMode(output2, OUTPUT);
+    pinMode(output15, OUTPUT);
 
  
- pinMode(input36,INPUT);
- pinMode(input39,INPUT);
- pinMode(input34,INPUT);
- pinMode(input35,INPUT);
- pinMode(input32,INPUT);
- pinMode(input33,INPUT);
- pinMode(input14,INPUT);
- pinMode(input12,INPUT);
- pinMode(input13,INPUT);
+   pinMode(input36,INPUT);
+   pinMode(input39,INPUT);
+   pinMode(input34,INPUT);
+   pinMode(input35,INPUT);
+   pinMode(input32,INPUT);
+   pinMode(input33,INPUT);
+   pinMode(input14,INPUT);
+   pinMode(input12,INPUT);
+   pinMode(input13,INPUT);
 
 xTaskCreatePinnedToCore( task_Speaker,
                               "Connect Speaker",
@@ -543,14 +536,14 @@ xTaskCreatePinnedToCore( task_Speaker,
                               2,
                               &task_Speaker_Handle,
                               app_cpu);
-     xTaskCreatePinnedToCore( task_Web,
-                                "Control_Web",
-                                2000,
-                                NULL,
-                                2,
-                                &task_Web_Handle,
-                                app_cpu);
-  
+xTaskCreatePinnedToCore( task_Web,
+                              "Control_Web",
+                              2000,
+                              NULL,
+                              2,
+                              &task_Web_Handle,
+                              app_cpu);
+
 }
 
 void loop(){
